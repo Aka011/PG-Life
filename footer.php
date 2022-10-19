b5a524a3f8fa8daef41d1fd23a9e08af5ba916bf
Ashu <116082240+Vishu0112@users.noreply.github.com>
2022-10-19 20:18:43 +0530

    Initial commit

---
+++ footer.php
@@ -0,0 +1,27 @@
+<div class="footer">
+        <div class="page-container footer-container">
+            <div class="footer-cities">
+                <div class="footer-city">
+                    <a href="Delhi-List.html">PG in Delhi</a>
+                </div>
+                <div class="footer-city">
+                    <a href="Mumbai-List.html">PG in Mumbai</a>
+                </div>
+                <div class="footer-city">
+                    <a href="Bangalore-List.html">PG in Bangaluru</a>
+                </div>
+                <div class="footer-city">
+                    <a href="Hyderabad-List.html">PG in Hyderabad</a>
+                </div>
+                <div class="footer-city">
+                    <a href="About.html">About Us</a>
+                </div>
+            </div>
+            <div class="footer-copyright">© 2020 Copyright PG Life </div>
+        </div>
+    </div>
+
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/common.js"></script>
---
+++ index.php
@@ -0,0 +1,94 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Welcome | PG Life</title>
+
+    <?php
+    include "head_links.php";
+    ?>
+    <link href="css/home.css" rel="stylesheet" />
+</head>
+
+<body>
+    <?php
+    include "header.php";
+    ?>
+     <div id="loading">
+    </div>
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb py-2">
+            <li class="breadcrumb-item">
+                <a href="index.php">Home</a>
+            </li>
+        </ol>
+    </nav>
+
+    <div class="banner-container">
+        <h2 class="white pb-3">Happiness per Square Foot</h2>
+
+        <form id="search-form" action="search.php" method="GET">
+            <div class="input-group city-search">
+                <input type="text" class="form-control input-city" id='city' name='city' placeholder="Enter your city to search for PGs" />
+                <div class="input-group-append">
+                    <button type="submit" name="city" class="btn btn-secondary">
+                        <i class="fa fa-search"></i>
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <div class="page-container">
+        <h1 class="city-heading">
+            Major Cities
+        </h1>
+        <div class="row">
+            <div class="city-card-container col-md">
+                <a href="Delhi-List.html">
+                    <div class="city-card rounded-circle">
+                        <img src="img/delhi.png" class="city-img" />
+                    </div>
+                </a>
+            </div>
+
+            <div class="city-card-container col-md">
+                <a href="Mumbai-List.html">
+                    <div class="city-card rounded-circle">
+                        <img src="img/mumbai.png" class="city-img" />
+                    </div>
+                </a>
+            </div>
+
+            <div class="city-card-container col-md">
+                <a href="Bangalore-List.html">
+                    <div class="city-card rounded-circle">
+                        <img src="img/bangalore.png" class="city-img" />
+                    </div>
+                </a>
+            </div>
+
+            <div class="city-card-container col-md">
+                <a href="Hyderabad-List.html">
+                    <div class="city-card rounded-circle">
+                        <img src="img/hyderabad.png" class="city-img" />
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <?php
+    include "signup_modal.php";
+    include "login_modal.php";
+    include "footer.php";
+    ?>
+
+</body>
+
+</html>
